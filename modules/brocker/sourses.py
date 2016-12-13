2943134f7b5194f170e981184848f64f1f746be3
Timofeeva Margarita <margaritachka@gmail.com>
2016-12-13 10:15:37 +0300

    Корректировка базового класса refs #10470

--- modules/brocker/sourses.py
+++ modules/brocker/sourses.py
@@ -13,41 +13,24 @@ class BaseClass(type):
 
     def __new__(cls, name, bases, attrs):
         new_cls = super(BaseClass, cls).__new__(cls, name, bases, attrs)
-        all_functions = [
-            (x, y) for x, y in attrs.items() if isinstance(y, FunctionType)
-        ]
 
-        cls.set_all_callbacks(new_cls, all_functions)
-        cls.set_all_signals(new_cls, all_functions)
+        callbacks = []
+        signals = []
+        for name_attr, attr in attrs.items():
+            # проверяем только атрибуты типа функции
+            if not isinstance(attr, FunctionType):
+                continue
+
+            # расширяем списки коллбэков и сигналов функциями
+            # с соответствующими атрибутами
+            if hasattr(attr, 'is_callback'):
+                callbacks.append(attr)
+            if hasattr(attr, 'is_signal'):
+                signals.append(attr)
+
+        new_cls.all_callbacks = callbacks
+        new_cls.all_signals = signals
         return new_cls
-
-    @classmethod
-    def set_all_callbacks(cls, child_cls, all_functions):
-        """
-            Устанавливает список коллбэков класса
-        """
-        child_cls.all_callbacks = cls.get_functions_by_attr(
-            'is_callback', all_functions
-        )
-
-    @classmethod
-    def set_all_signals(cls, child_cls, all_functions):
-        """
-            Устанавливает список сигналов класса
-        """
-        child_cls.all_signals = cls.get_functions_by_attr(
-            'is_signal', all_functions
-        )
-
-    @classmethod
-    def get_functions_by_attr(cls, attr_name, all_functions):
-        """
-           Возвращает список функций, у которых есть атрибут attr_name
-        """
-        return [
-            func for f_name, func in all_functions
-                if hasattr(func, attr_name)
-        ]
 
     def get_all_callbacks(cls):
         """
