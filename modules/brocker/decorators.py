bd2203f189a99b0a6efbc8e22f160362077dd6a1
Timofeeva Margarita <margaritachka@gmail.com>
2016-12-13 09:22:47 +0300

    Добавили декоратор, устанавливающий атрибут функции refs #10470

--- modules/brocker/decorators.py
+++ modules/brocker/decorators.py
@@ -3,14 +3,12 @@
 from functools import wraps
 
 
-def signal(max_retries=1, expire=60, routing_key='default'):
+def signal(*args_signal, **kwargs_signal):
     """
         Декоратор для функции-сигнала
     """
     def decorator(func):
-        func.is_signal = True
-
-        @wraps(func)
+        @wraps(extend_func(func, 'is_signal'))
         def wrapper(self, *args, **kwargs):
             func(self, *args, **kwargs)
         return wrapper
@@ -21,9 +19,18 @@ def callback(func):
     """
         Декоратор для функции-коллбэка
     """
-    func.is_callback = True
-
-    @wraps(func)
+    @wraps(extend_func(func, 'is_callback'))
     def wrapper(self, *args, **kwargs):
         return func(self, *args, **kwargs)
     return wrapper
+
+
+def extend_func(func, attr_name, attr_val=True):
+    """
+        Устанавливает значение конкретного атрибута функции
+            func - функция
+            attr_name - имя атрибута
+            attr_val - значение атрибута
+    """
+    setattr(func, attr_name, attr_val)
+    return func
