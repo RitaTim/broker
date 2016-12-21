7a9702ce80e5a45ded46cb426d0a6dd395594e01
Timofeeva Margarita <margaritachka@gmail.com>
2016-12-21 16:34:27 +0300

    Правки по ревью (Rule) 2 refs #10525

--- modules/main/static/broker/js/rule_actions.js
+++ modules/main/static/broker/js/rule_actions.js
@@ -1,57 +1,20 @@
 /*
    Логика работы с объектом Rule брокера в админке
 */
-(function($){
-        var set_source = function(){
-            // Меняем значение текущего источника и значение источника в скрытом поле
-            Rule.source = Rule.widgets.source_select.val() || Rule.source;
-            Rule.widgets.source_hidden_input.val(Rule.source);
-        };
+function ruleManager(){
+    this.get_data_source_methods = '/broker/source/get_methods';
+};
 
-        var set_destination = function () {
-            // Меняем значение текущего приемника и значение приемника в скрытом поле
-            Rule.destination = Rule.widgets.destination_select.val() || Rule.destination;
-            Rule.widgets.destination_hidden_input.val(Rule.destination);
-        };
-
-        var update_select_methods = function(select, source, type_methods){
-            // Обновляем селект с сигналами или коллбэками
-            if (!source){
-                select.html("");
-                return;
+ruleManager.prototype.get_methods_for_cls = function(source, type_methods){
+    $.getJSON(
+        [this.get_data_source_methods, type_methods, source].join("/"),
+        function (data) {
+            if (data[type_methods]) {
+                $.event.trigger('Rule.update_select', {
+                    'type_methods': type_methods,
+                    'methods': data[type_methods]
+                });
             }
-
-            var GET_DATA_SOURCE_URL = '/broker/source/get_methods';
-            $.getJSON(
-                [GET_DATA_SOURCE_URL, type_methods, source].join("/"),
-                function (data) {
-                    var options = "";
-                    var current_func = type_methods == 'callbacks' ? Rule.callback : Rule.signal;
-                    $.each(data[type_methods], function(num, func){
-                        options = options + '<option value="' + func + '" ' + (func == current_func ? "selected" : "") + ' >' + func + '</option>';
-                    });
-                    select.html(options);
-                }
-            );
-        };
-
-    $(function() {
-        Rule.init();
-        var $_Rule = $(Rule);
-
-        // Навешиваем обработчики событий
-        $_Rule.on('RuleForm.change_source', function(){
-            set_source();
-            update_select_methods(Rule.widgets.signal_select, Rule.source, 'signals');
-        });
-
-        $_Rule.on('RuleForm.change_destination', function(){
-            set_destination();
-            update_select_methods(Rule.widgets.callback_select, Rule.destination, 'callbacks');
-        });
-        
-        // Генирируем события, для обновления селектов сигналов и коллбэков
-        Rule.change_source_event();
-        Rule.change_destination_event();
-    });
-})(jQuery);
+        }
+    );
+};
--- modules/main/static/broker/js/rule_form.js
+++ modules/main/static/broker/js/rule_form.js
@@ -30,11 +30,11 @@ var Rule = {
     // Генерация событий
     'change_source_event': function(){
         // Изменился источник
-        $(Rule).trigger('RuleForm.change_source');
+        $.event.trigger('Rule.change_source');
     },
     'change_destination_event': function(){
         // Изменился приемник
-        $(Rule).trigger('RuleForm.change_destination');
+        $.event.trigger('Rule.change_destination');
     },
 
     'init': function(){
@@ -45,9 +45,58 @@ var Rule = {
         Rule.destination = Rule.widgets.destination_hidden_input.val();
         Rule.callback = Rule.widgets.callback_hidden_input.val();
         Rule.signal = Rule.widgets.signal_hidden_input.val();
-
-        // Навешиваем обработчики на событие смены источника и приемника
-        Rule.widgets.source_select.on('change', Rule.change_source_event);
-        Rule.widgets.destination_select.on('change', Rule.change_destination_event);
     }
 };
+
+jQuery(document).ready(function(){
+    Rule.init();
+
+    Rule.widgets.source_select.on('change', Rule.change_source_event);
+    Rule.widgets.destination_select.on('change', Rule.change_destination_event);
+
+    // Генирируем события, для обновления селектов сигналов и коллбэков
+    if (Rule.source && Rule.destination) {
+        Rule.change_source_event();
+        Rule.change_destination_event();
+    }
+});
+
+
+jQuery(document).bind({
+    'Rule.change_source': function(){
+        Rule.source = Rule.widgets.source_select.val() || Rule.source;
+        Rule.widgets.source_hidden_input.val(Rule.source);
+
+        var ruleMan = new ruleManager();
+        ruleMan.get_methods_for_cls(Rule.source, 'signals')
+    },
+    'Rule.change_destination': function(){
+        Rule.destination = Rule.widgets.destination_select.val() || Rule.destination;
+        Rule.widgets.destination_hidden_input.val(Rule.destination);
+
+        var ruleMan = new ruleManager();
+        ruleMan.get_methods_for_cls(Rule.destination, 'callbacks')
+    },
+    'Rule.update_select': function(event, data){
+        var current_func;
+        var select;
+        var type_methods = data['type_methods'];
+        var methods = data['methods'];
+
+        if (type_methods == 'callbacks'){
+            current_func = Rule.callback;
+            select = Rule.widgets.callback_select;
+        } else {
+            current_func = Rule.signal;
+            select = Rule.widgets.signal_select;
+        }
+
+        // Формируем список опций для селекта
+        var options = "";
+        $.each(methods, function(num, func){
+            options = options + '<option value="' + func + '" ' + (func == current_func ? "selected" : "") + ' >' + func + '</option>';
+        });
+
+        select.html(options);
+    }
+});
