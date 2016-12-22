2a83fd2a99fbc9e04b5103e033c497a9f82e4522
Timofeeva Margarita <margaritachka@gmail.com>
2016-12-22 14:47:54 +0300

    (Rule) Добавили проверку на уникальность полей при редактировании формы refs #10525

--- modules/broker/forms.py
+++ modules/broker/forms.py
@@ -12,19 +12,27 @@ class RuleAdminForm(forms.ModelForm):
         Форма в админке для создания и редактирования правил брокера
     """
     def clean(self):
-        result = super(RuleAdminForm, self).clean()
+        cleaned_data = super(RuleAdminForm, self).clean()
+        signal = cleaned_data.get('signal')
+        callback = cleaned_data.get('callback')
 
         if self.instance.id:
             source = self.instance.source
             destination = self.instance.destination
+
+            # Проверка на существование правила с указанными параметрами
+            if Rule.objects.filter(source=source, destination=destination,
+                                   signal=signal, callback=callback)\
+                           .exclude(id=self.instance.id).exists():
+                raise forms.ValidationError(
+                    u'Rule c указанными параметрами уже существует'
+                )
         else:
             source = self.cleaned_data.get('source')
             destination = self.cleaned_data.get('destination')
 
+        # Проверка на наличие сигнала и коллбэка у источника
         if source and destination:
-            signal = self.cleaned_data.get('signal')
-            callback = self.cleaned_data.get('callback')
-
             if signal:
                 # Проверка, содержит ли источник такой сигнал
                 self._check_cls_has_func(source.source, signal)
@@ -33,7 +41,8 @@ class RuleAdminForm(forms.ModelForm):
                 self._check_cls_has_func(
                     destination.source, callback, 'callbacks'
                 )
-        return result
+
+        return cleaned_data
 
     def _check_cls_has_func(self, cls_name, func_name, type_func='signals'):
         """
