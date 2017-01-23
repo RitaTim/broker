773d968a65b24d176df83b9cb4ecbfd95b9d87f3
Timofeeva Margarita <margaritachka@gmail.com>
2017-01-23 09:11:34 +0300

    Поправили формат даты и времени, используемых при получении отчета refs #10725

--- modules/broker/sources/wsdl/sources.py
+++ modules/broker/sources/wsdl/sources.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
+
 from broker.sources.wsdl import Wsdl
 from broker.decorators.decorators import callback, signal
 
@@ -38,15 +40,16 @@ class OneSWsdl(Wsdl):
         # Устанавливаем состояние в P - process
         self.change_task_state_buffer(task_id=kwargs['task_id'], state='P')
 
+        date_f = "%Y%m%d"
         result = dict(self.wsdl_client.service.ReportEquipmentRepairStatus2(
-            kwargs['task_id'], kwargs['uuid'], kwargs['start_date'],
-            kwargs['end_date'], kwargs['email']
+            kwargs['task_id'], kwargs['uuid'],
+            datetime.strptime(kwargs['start_date'], date_f),
+            datetime.strptime(kwargs['end_date'], date_f), kwargs['email']
         ))
         # Кидаем сигнал о том, что отчет получен
 
-        time_out = 20161120100813 # todo: haha
         self.received_report_equipment_repair(
             task_id=kwargs['task_id'], data=result.get('return'), state='F',
-            time_out=time_out
+            time_out=datetime.now().strftime("%Y%m%d%I%M%S")
         )
         return result
